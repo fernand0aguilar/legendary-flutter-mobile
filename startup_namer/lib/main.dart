37eacb4f3abbec1de37107855418f6c0884e9b77
Fernando Aguilar <fernando@aguilar.net.br>
2018-12-18 16:24:43 -0300

    Add Interactive Icons to the list

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -16,13 +16,30 @@ class MyApp extends StatelessWidget {
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
+  final _saved = new Set<WordPair>();
 
  Widget _buildRow(WordPair pair) {
+   final bool alreadySaved = _saved.contains(pair);
+
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if(alreadySaved) {
+            _saved.remove(pair);
+          }
+          else {
+            _saved.add(pair);
+          }
+        });
+      }
     );
   }
   
